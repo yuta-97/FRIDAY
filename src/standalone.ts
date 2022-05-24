c87509bb82ffea59719955e5c29002ab2d736a79
rkdwn <kj2693119@gmail.com>
2022-05-24 16:41:32 +0900

    [Fix] deploy settings & dockerfiles, module import error

--- src/standalone.ts
+++ src/standalone.ts
@@ -1,4 +1,22 @@
-// TODO: build 시에 import error 해결하기
+/**
+ * PRE-requisite function.
+ *
+ * check env files and set module import method.
+ */
+import dotenv from "dotenv";
+const envFound = dotenv.config();
+
+if (envFound.error) {
+  throw new Error(" Could't find .env file... ");
+}
+
+import moduleAlias from "module-alias";
+moduleAlias.addAlias("@", __dirname);
+
+/**
+ * Main Loop here.
+ */
+import "reflect-metadata"; // We need this in order to use @Decorators
 import BotGenerator from "@/singleton/bot";
 import filterCommand from "@/commands";
 
